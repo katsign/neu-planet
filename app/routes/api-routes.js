d9dc5304ad63765352c15817293e33c52faba151
Kat Buchanan <katrbuch@gmail.com>
2021-03-31 15:04:28 -0500

    working on delete posts

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,14 +1,14 @@
+const express = require("express");
+const router = express.Router();
 const Post = require("../models/post.js");
 
-// Routes
-module.exports = (app) => {
   // Get All
-  app.get("/api/all", (req, res) => {
+  router.get("/api/all", (req, res) => {
     Post.findAll({}).then((results) => res.json(results));
   });
 
   // Add
-  app.post("/api/new", (req, res) => {
+  router.post("/api/new", (req, res) => {
     console.log("Post Data:");
     console.log(req.body);
 
@@ -18,4 +18,16 @@ module.exports = (app) => {
       created_at: req.body.created_at,
     }).then((results) => res.json(results));
   });
-};
+
+  // Delete
+  router.delete('/api/posts/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+    burger.delete(condition, (result) => {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    });
+  });
+
+module.exports = router;
